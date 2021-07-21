8ab5215f346285e5d7493a194a089adc51fe75ce
Juan Osorio <juan.michael.osorio@gmail.com>
2021-07-21 11:21:54 -0500

    fixed mongo_URI

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/employees', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/employees', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
